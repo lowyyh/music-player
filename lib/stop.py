45bcff238ffe448acee049c32e3764965c00e0ce
lowyyh <lowyyh.com@outlook.com>
2024-01-14 16:15:38 +0800

    第20次提交 解决一个小bug

---
+++ lib/stop.py
@@ -0,0 +1,22 @@
+import ctypes
+import inspect
+
+
+def async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        # raise ValueError("invalid thread id")
+        return
+    if res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
+
+def stop_thread(thread):
+    async_raise(thread.ident, SystemExit)

c7f5c29d840b1356781f46e4f11dcb565b3ea1d1
lowyyh <lowyyh.com@outlook.com>
2024-01-31 11:25:25 +0800

    github

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2024 lowyyh
+Copyrig
 音乐播放器 is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
